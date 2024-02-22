7649143f65ef45093dfaa7a1975ba83f5ff9ebee
madiyarrr1 <rahmangalim@gmail.com>
2024-02-23 04:28:56 +0600

    a

---
+++ settings.py
@@ -0,0 +1,24 @@
+DEFAULT_IMAGE_SIZE = (300, 300)
+FPS = 120
+HEIGHT = 1000
+WIDTH = 962
+START_X, START_Y = 0, -300
+X_OFFSET, Y_OFFSET = 20, 0
+
+BG_IMAGE_PATH = 'graphics/0/bg.png'
+GRID_IMAGE_PATH = 'graphics/0/gridline.png'
+GAME_INDICES = [1, 2, 3]
+SYM_PATH = 'graphics/0/symbols'
+
+TEXT_COLOR = 'White'
+UI_FONT = 'graphics/font/Casino.ttf'
+UI_FONT_SIZE = 20
+WIN_FONT_SIZE = 40
+
+symbols = {
+    'crown': f"{SYM_PATH}/crown.png",
+    'chip': f"{SYM_PATH}/chip.png",
+    'seven': f"{SYM_PATH}/seven.png",
+    'cherry': f"{SYM_PATH}/cherry.png",
+    'dices': f"{SYM_PATH}/dices.png"
+}
